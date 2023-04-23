acc66e8b15b8d80e606853661afd9e7bfdb585ec
Philip Thomas <110606536+PhilipThomas6w@users.noreply.github.com>
2023-04-23 20:54:48 +0100

    Added DelegatesDemo3

---
+++ 12. Events and Delegates/DelegatesDemo3/DelegatesDemo3/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace Coding.Exercise;
+
+public class Run
+{
+    public delegate float OperationDelegate(float a, float b);
+
+    public static float Add(float a, float b)
+    {
+        return a + b;
+    }
+
+    public static float Subtract(float a, float b)
+    {
+        return a - b;
+    }
+
+
+    public static float Multiply(float a, float b)
+    {
+        return a * b;
+    }
+
+    public static float Divide(float a, float b)
+    {
+        return a / b;
+    }
+
+    public static float ApplyOperation(float a, float b, OperationDelegate oper)
+    {
+        float result = oper(a, b);
+        return result;
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine("Enter a number: ");
+        string userInput1 = Console.ReadLine();
+        float a = float.Parse(userInput1);
+        Console.WriteLine("Enter another number: ");
+        string userInput2 = Console.ReadLine();
+        float b = float.Parse(userInput2);
+      
+        Console.WriteLine(ApplyOperation(a,b, Multiply));
+
+    }
+}
