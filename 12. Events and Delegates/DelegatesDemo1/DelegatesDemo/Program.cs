4676ee0f5ab4f94328ae888f334a12b1fc4ce882
Philip Thomas <110606536+PhilipThomas6w@users.noreply.github.com>
2023-04-23 19:44:22 +0100

    Added DelegatesDemo1 and DelegatesDemo2

---
+++ 12. Events and Delegates/DelegatesDemo1/DelegatesDemo/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace DelegatesDemo;
+
+internal class Program
+{
+    static void Main()
+    {
+        // Declare a list of names and initialise
+
+        List<string> names = new() { "Aiden", "Sif", "Walter", "Anatoli" };
+
+        // Print the names before we run the RemoveAll method.
+        
+        Console.WriteLine("---before---");
+        
+        foreach (var name in names)
+        {
+            Console.WriteLine(name);
+        }
+
+        // Now we call the RemoveAll method. The RemoveAll method takes a predicate (i.e., a type of delegate) as an argument; in this case, we will pass a method we've defined below, named Filter, as the predicate. The return type of a predicate is a bool.
+        
+        names.RemoveAll(Filter);    // Removes each element in names for which Filter returns true.
+
+        // Print the names after we run the RemoveAll method.
+        
+        Console.WriteLine("---after---");
+
+        foreach (var name in names)
+        {
+            Console.WriteLine(name);
+        }
+
+    }
+
+    static bool Filter(string s)
+    {
+        return s.Contains("i"); // If string s contains "i", return true, else false.
+    }
+
+}
